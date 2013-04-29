3b7e112a193dc4f65e57a771a6f25197eb75229d
Weyert de Boer <weyert.deboer@keytree.co.uk>
2013-04-29 08:54:57 +0100

    Reworking my personal settings files

--- modules/people/manifests/weyert.pp
+++ modules/people/manifests/weyert.pp
@@ -1,9 +1,23 @@
 include people::weyert::applications
-include people::weyert::osx_defaults
 
 class people::weyert {
-  include homebrew
+  include people::weyert::params
+  include people::weyert::applications
+  include people::weyert::repositories
+  include people::weyert::config
 
+  notify { 'class people::weyert declared': }
   $home      = "/Users/${::luser}"
-  $devfolder = "${home}/my"
+  $devfolder = "${home}/Development"
+  $dotfiles_dir = "${boxen::config::srcdir}/.oh-my-zsh"
+
+  repository { $dotfiles_dir:
+    source => "${::github_user}/oh-my-zsh"
+  }
+
+  file { "${home}/.zshrc":
+    ensure  => link,
+    target  => "${dotfiles_dir}/.zshrc",
+    require => Repository[$dotfiles_dir]
+  }
 }
