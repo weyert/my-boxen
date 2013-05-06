c76d6d064e780b6129025927b44a105a2e94c521
Weyert de Boer <weyertdeboer@Weyerts-MacBook-Pro.local>
2013-05-06 20:19:50 +0100

    Experimentation to get Evernote installed too'

--- modules/people/manifests/weyert/applications.pp
+++ modules/people/manifests/weyert/applications.pp
@@ -26,8 +26,18 @@ class people::weyert::applications {
       provider => appdmg,
     }
 
+    package { 'Rucksack':
+	source 	 => 'http://commondatastorage.googleapis.com/incrediblebee/apps/Archiver/Archiver-2385.zip',
+	provider => compressed_app
+    }
+
     package { 'VirtualHostX':
       source   => 'http://dl.clickontyler.com/virtualhostx/virtualhostx40_4.0.6.zip',
       provider => 'compressed_app'
+    }
+
+    package { 'Evernote':
+	source => 'http://cdn1.evernote.com/mac/release/Evernote_400995.dmg'
+        provider => 'appdmg_eula'
     }
 }
