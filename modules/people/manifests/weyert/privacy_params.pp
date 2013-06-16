4ddb656b7577cb76a7e73162ecf4cb8f99fe5c29
Weyert de Boer <weyert.deboer@keytree.co.uk>
2013-06-16 14:58:47 +0100

    Code formatting updates

--- modules/people/manifests/weyert/privacy_params.pp
+++ modules/people/manifests/weyert/privacy_params.pp
@@ -1,7 +1,5 @@
 class people::weyert::privacy_params {
-  # $::luser and $::boxen_srcdir come from Boxen's custom facts
-#  $my_phonenumber = '+44 74 476 09 086'
-
   # Recovery message
-  osx::recovery_message { "If this Mac is found, please call and collect your reward +44 74 476 09 086": }
+  	osx::recovery_message { "If this Mac is found, please call and collect your reward +44 74 476 09 086": 
+  }
 }
